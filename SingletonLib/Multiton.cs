7bff2a79ce197a920d2e31fc4a860c2cfac8a441
Дмитрий <semu-dima@yandex.ru>
2022-06-23 14:01:51 +0300

    добавлен мультитон

---
+++ SingletonLib/Multiton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Singleton_Multiton
+{
+    public class Multiton
+    {
+        private static Dictionary<string, Multiton> _instances = new();
+        private static Multiton _multitonInstance = null;
+
+        public static Multiton Instance(string key)
+        {
+            _multitonInstance = _instances[key];
+            if (_multitonInstance is null)
+            {
+                _multitonInstance = new Multiton();
+                _instances.Add(key, _multitonInstance);
+            }
+
+            return _multitonInstance;
+        }
+    }
+}

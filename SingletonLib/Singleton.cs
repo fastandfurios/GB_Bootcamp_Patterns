2915e77bba44649d0c8e63b4b4c71eb4ff8e624d
Дмитрий <semu-dima@yandex.ru>
2022-06-23 08:00:22 +0300

    добавил класс, добавил код

---
+++ SingletonLib/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace SingletonLib
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+
+        public static Singleton Instance()
+        {
+            //return _instance ??= new Singleton();
+
+            if( _instance == null )
+                _instance = new Singleton();
+
+            return _instance;
+        }
+    }
+}

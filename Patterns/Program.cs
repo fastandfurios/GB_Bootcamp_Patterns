963a4e0ffd696d58d4e5f17c3dd3245f18a4574c
Дмитрий <semu-dima@yandex.ru>
2022-06-24 20:17:55 +0300

    изменения в коде

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Singleton_Multiton;
+using Factory_method;
 
 var singleton = Singleton.Instance();
 var singleton2 = Singleton.Instance();
@@ -9,5 +10,17 @@ multiton = Multiton.Instance("2");
 var limitedMultiton = LimitedMultiton.Instance(LimitedMultiton.Count.ONE);
 limitedMultiton = LimitedMultiton.Instance(LimitedMultiton.Count.TWO);
 
+Console.WriteLine(singleton.Equals(singleton2));
 
-    Console.WriteLine(singleton.Equals(singleton2));
+//***********************************************
+
+IStore store = new GroceryStore();
+store.SaleGood();
+
+IStoreFactory storeFactory = new GroceryStoreFactory();
+var storeGrocery = storeFactory.CreateStore();
+storeGrocery.SaleGood();
+
+IStoreFactory storeFactoryWear = new WearStoreFactory();
+var storeWear = storeFactoryWear.CreateStore();
+storeWear.SaleGood();

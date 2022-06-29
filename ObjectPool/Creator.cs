8976cbb7dd703cca3d71f919ee1bc1231a6adc0f
Дмитрий <semu-dima@yandex.ru>
2022-06-29 21:10:04 +0300

    добавил пример паттерна пулл объектов

---
+++ ObjectPool/Creator.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectPool
+{
+    public class Creator : ICreation<Reusable>
+    {
+        private static int _id = 0;
+
+        public Reusable Create()
+        {
+            ++_id;
+            return new(_id);
+        }
+    }
+}

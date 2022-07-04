b2aec2617c6529454a51edf9b265dc388a77556c
Дмитрий <semu-dima@yandex.ru>
2022-07-04 10:10:53 +0300

    добавил декоратор

---
+++ Decorator_pattern/ConcreteDecoratorB.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator_pattern
+{
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("ConcreteDecoratorB.Operation()");
+        }
+
+        private void AddedBehavior()
+        {
+            //code
+        }
+    }
+}
---
+++ Decorator_pattern/MainClass.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator_pattern
+{
+    public static class MainClass
+    {
+        public static void Run()
+        {
+            var concreteComponent = new ConcreteComponent();
+            var concreteDecoratorA = new ConcreteDecoratorA();
+            var concreteDecoratorB = new ConcreteDecoratorB();
+
+            concreteDecoratorA.SetComponent(concreteComponent);
+            concreteDecoratorB.SetComponent(concreteDecoratorA);
+
+            concreteDecoratorB.Operation();
+        }
+    }
+}

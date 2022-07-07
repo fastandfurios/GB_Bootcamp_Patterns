53c67aabc5a763ef07c57320713700d836cb4efa
Дмитрий <semu-dima@yandex.ru>
2022-07-07 07:51:45 +0300

    добавил пример паттерна медиатор

---
+++ Mediator_pattern/ConcreteColleague1.cs
@@ -0,0 +1,18 @@
+﻿namespace Mediator_pattern;
+
+public class ConcreteColleague1 : Colleague
+{
+    public ConcreteColleague1(Mediator mediator) : base(mediator)
+    {
+    }
+
+    public void Send(string message)
+    {
+        _mediator.Send(message, this);
+    }
+
+    public void Notify(string message)
+    {
+        Console.WriteLine($"Colleague1 gets message: {message}");
+    }
+}
---
+++ Mediator_pattern/Mediator.cs
@@ -0,0 +1,7 @@
+﻿namespace Mediator_pattern
+{
+    public abstract class Mediator
+    {
+        public abstract void Send(string message, Colleague colleague);
+    }
+}
